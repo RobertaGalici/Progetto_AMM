ea8ec0baa82bd2a8bf6d393b78e79c1632020b87
Robi <Robi@10.50.138.171>
2017-04-22 13:46:55 +0200

    M3: task 2: factory

--- ProgettoAMM/src/java/amm/nerdbook/classi/PostFactory.java
+++ ProgettoAMM/src/java/amm/nerdbook/classi/PostFactory.java
@@ -9,6 +9,76 @@ package amm.nerdbook.classi;
  *
  * @author Robi
  */
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class PostFactory {
-    
+
+    //Pattern Design Singleton
+    private static PostFactory singleton;
+
+    public static PostFactory getInstance() {
+        if (singleton == null) {
+            singleton = new PostFactory();
+        }
+        return singleton;
+    }
+
+    private ArrayList<Post> listaPost = new ArrayList<Post>();
+
+    private PostFactory() {
+        
+        UtentiRegistratiFactory utentiRegistratiFactory = UtentiRegistratiFactory.getInstance();
+
+        //Creazione Post
+        Post post1 = new Post();
+        post1.setContent("Luigi, ti aspetto in pista!");
+        post1.setId(0);
+        post1.setUser(utentiRegistratiFactory.getUtentiRegistratiById(0));
+
+        Post post2 = new Post();
+        post2.setContent("Preparati ad affrontare la mia ira, Crash Bandicoot!");
+        post2.setId(1);
+        post2.setUser(utentiRegistratiFactory.getUtentiRegistratiById(0));
+        post2.setPostType(Post.Type.IMAGE);
+        
+        Post post3 = new Post();
+        post3.setContent("Ehi Cortex, dai un'occhiata qua: https://www.youtube.com/watch?v=DYjcZ5e5-iM");
+        post3.setId(2);
+        post3.setUser(utentiRegistratiFactory.getUtentiRegistratiById(1));
+        
+        Post post4 = new Post();
+        post4.setContent("Mario, guarda come eravamo giovani!");
+        post4.setId(3);
+        post4.setUser(utentiRegistratiFactory.getUtentiRegistratiById(1));
+        post4.setPostType(Post.Type.IMAGE);
+
+        listaPost.add(post1);
+        listaPost.add(post2);
+        listaPost.add(post3);
+        listaPost.add(post4);
+    }
+
+    public Post getPostById(int id) {
+        for (Post post : this.listaPost) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
+        return null;
+    }
+
+    public List getPostList(UtentiRegistrati ureg) {
+
+        List<Post> listaPost = new ArrayList<Post>();
+
+        for (Post post : this.listaPost) {
+            if (post.getUser().equals(ureg)) {
+                listaPost.add(post);
+            }
+        }
+        return listaPost;
+    }
 }
+
--- ProgettoAMM/src/java/amm/nerdbook/classi/UtentiRegistratiFactory.java
+++ ProgettoAMM/src/java/amm/nerdbook/classi/UtentiRegistratiFactory.java
@@ -4,11 +4,83 @@
  * and open the template in the editor.
  */
 package amm.nerdbook.classi;
-
+import java.util.ArrayList;
 /**
  *
  * @author Robi
  */
 public class UtentiRegistratiFactory {
-    
+
+    //Pattern Design Singleton
+    private static UtentiRegistratiFactory singleton;
+
+    public static UtentiRegistratiFactory getInstance() {
+        if (singleton == null) {
+            singleton = new UtentiRegistratiFactory();
+        }
+        return singleton;
+    }
+
+    private ArrayList<UtentiRegistrati> listaUtentiRegistrati = new ArrayList<UtentiRegistrati>();
+
+    private UtentiRegistratiFactory() {
+        //Creazione utenti
+
+        //Mario
+        UtentiRegistrati utente1 = new UtentiRegistrati();
+        utente1.setId(0);
+        utente1.setNome("Mario");
+        utente1.setCognome("Bros");
+        utente1.setEmail("mariobros@gmail.com");
+        utente1.setDataNascita("10/03/1900");
+        utente1.setPassword("0000");
+        utente1.setUrlProfilo("img/SuperMario.jpg");
+        utente1.setPresentazione("Ciao, sono Mario!");
+
+        //Luigi
+        UtentiRegistrati utente2 = new UtentiRegistrati();
+        utente2.setId(1);
+        utente2.setNome("Luigi");
+        utente2.setCognome("Bros");
+        utente2.setEmail("luigino@gmail.com");
+        utente2.setDataNascita("16/07/1930");
+        utente2.setPassword("1234");
+        utente2.setUrlProfilo("");
+        utente2.setPresentazione("Ciao, sono Luigi!");
+        
+        //Crash Bandicoot
+        UtentiRegistrati utente3 = new UtentiRegistrati();
+        utente3.setId(2);
+        utente3.setNome("Crash");
+        utente3.setCognome("Bandicoot");
+        utente3.setEmail("crashbandicoot@gmail.com");
+        utente3.setPassword("5678");
+        utente3.setUrlProfilo("");
+        utente3.setPresentazione("Ciao, sono Crash!");
+        
+        //Neo Cortex
+        UtentiRegistrati utente4 = new UtentiRegistrati();
+        utente4.setId(2);
+        utente4.setNome("Neo");
+        utente4.setCognome("Cortex");
+        utente4.setEmail("cortex@gmail.com");
+        utente4.setPassword("9001");
+        utente4.setUrlProfilo("");
+        utente4.setPresentazione("Ciao, sono Cortex e sono un super cattivo! Mhuahahahah");
+       
+
+        listaUtentiRegistrati.add(utente1);
+        listaUtentiRegistrati.add(utente2);
+        listaUtentiRegistrati.add(utente3);
+        listaUtentiRegistrati.add(utente4);
+    }
+
+    public UtentiRegistrati getUtentiRegistratiById(int id) {
+        for (UtentiRegistrati user : this.listaUtentiRegistrati) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
